84bdba9317fca30607f72f6e8eeff95d01e90525
salashine <sxznpc@163.com>
2016-06-29 22:24:15 +0800

    add new files..

---
+++ module1/src/VMStackSOF.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sala on 16/6/10.
+ */
+public class VMStackSOF {
+    private int stackLenght = 1;
+    public void stackLeak(){
+        stackLenght++;
+        stackLeak();
+    }
+
+    public static void main(String[] args) {
+        VMStackSOF oom = new VMStackSOF();
+        try {
+            oom.stackLeak();
+        } catch (Exception e) {
+            System.out.println("stackLenght:" + oom.stackLenght);
+            //e.printStackTrace();
+        }
+    }
+}
